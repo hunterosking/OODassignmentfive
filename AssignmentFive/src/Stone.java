aea9a51e21b90f714e279c155822c6229af32779
Evilsnowflake <Evilsnowflake1@yahoo.com>
2016-11-10 12:10:43 -0500

    Adjusted AI player, began implementing stone class for creating walls

---
+++ AssignmentFive/src/Stone.java
@@ -0,0 +1,9 @@
+
+public class Stone {
+	public Player owner; //who owns this stone?
+	public int type; //what type of stone is it? wall?
+	public Stone(Player owner, int type){
+		this.owner = owner;
+		this.type = type;
+	}
+}
